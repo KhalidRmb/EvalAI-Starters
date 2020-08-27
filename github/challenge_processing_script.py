0722d144dd92160f4b02f4f509500f32addb44fc
Khalid Riyaz <Khalidriyazgit@gmail.com>
2020-08-27 20:38:04 +0530

    Update challenge_processing_script.py

--- github/challenge_processing_script.py
+++ github/challenge_processing_script.py
@@ -6,9 +6,12 @@ import sys
 
 from config import *
 from utils import (
-	create_issue_in_repo,
-	comment_on_pr,
-	construct_challenge_zip_file,
+	add_pull_request_comment,
+	check_for_errors,
+	check_if_merge_or_commit,
+	check_if_pull_request,
+	create_challenge_zip_file,
+	create_github_repository_issue,
 	get_request_header,
 	load_host_configs,
 	validate_token,
@@ -20,21 +23,11 @@ HOST_AUTH_TOKEN = None
 CHALLENGE_HOST_TEAM_PK = None
 EVALAI_HOST_URL = None
 
-os.environ["CHALLENGE_ERRORS"] = "False"
-
-IS_VALIDATION = os.getenv("IS_VALIDATION")
-GITHUB_EVENT_NAME = os.getenv("GITHUB_EVENT_NAME")
-
 GITHUB_CONTEXT = json.loads(os.getenv("GITHUB_CONTEXT"))
 GITHUB_AUTH_TOKEN = os.getenv("GITHUB_AUTH_TOKEN")
 
-if GITHUB_EVENT_NAME.startswith("pull_request"):
-	PR_NUMBER = GITHUB_CONTEXT["event"]["number"]
 
 if __name__ == "__main__":
-
-	if IS_VALIDATION == "False" and GITHUB_EVENT_NAME.startswith("pull_request"):
-		sys.exit(0)
 
 	configs = load_host_configs(HOST_CONFIG_FILE_PATH)
 	if configs:
@@ -45,15 +38,15 @@ if __name__ == "__main__":
 		sys.exit(1)
 
 	# Fetching the url
-	if IS_VALIDATION == "True":
+	if VALIDATION_STEP == "True":
 		url = "{}{}".format(EVALAI_HOST_URL, CHALLENGE_CONFIG_VALIDATION_URL.format(CHALLENGE_HOST_TEAM_PK))
-	if IS_VALIDATION == "False" and GITHUB_EVENT_NAME == "push":
+	else:
 		url = "{}{}".format(EVALAI_HOST_URL, CHALLENGE_CREATE_OR_UPDATE_URL.format(CHALLENGE_HOST_TEAM_PK))
 
 	headers = get_request_header(HOST_AUTH_TOKEN)
 
 	# Creating the challenge zip file and storing in a dict to send to EvalAI
-	construct_challenge_zip_file(CHALLENGE_ZIP_FILE_PATH, IGNORE_DIRS, IGNORE_FILES)
+	create_challenge_zip_file(CHALLENGE_ZIP_FILE_PATH, IGNORE_DIRS, IGNORE_FILES)
 	zip_file = open(CHALLENGE_ZIP_FILE_PATH, 'rb')
 	file = {"zip_configuration": zip_file}
 
@@ -71,36 +64,36 @@ if __name__ == "__main__":
 			is_token_valid = validate_token(response.json())
 			if is_token_valid:
 				error = response.json()["error"]
-				error_message = "\nThere was were some errors in the challenge config:\n{}".format(error)
+				error_message = "\nFollowing errors occurred while validating the challenge config:\n{}".format(error)
 				print(error_message)
 				os.environ["CHALLENGE_ERRORS"] = error_message
 		else:
 			print("\nThere was an error: {}".format(err))
 			os.environ["CHALLENGE_ERRORS"] = str(err)
 	except Exception as e:
-		if IS_VALIDATION=="True":
-			error_message = "\nThere was an error when validating the challenge config: {}".format(e)
+		if VALIDATION_STEP=="True":
+			error_message = "\nFollowing errors occurred while validating the challenge config: {}".format(e)
 			print(error_message)
 			os.environ["CHALLENGE_ERRORS"] = error_message
 		else:
-			error_message = "\nThere was an error: {}".format(e)
+			error_message = "\nFollowing errors occurred while validating the challenge config: {}".format(e)
 			print(error_message)
 			os.environ["CHALLENGE_ERRORS"] = error_message
 
 	zip_file.close()
 	os.remove(zip_file.name)
 
-	if os.environ.get("CHALLENGE_ERRORS") != "False" and IS_VALIDATION=="True" and GITHUB_EVENT_NAME.startswith("pull_request"):
-		message = os.environ.get("CHALLENGE_ERRORS")
-		comment_on_pr(GITHUB_AUTH_TOKEN, os.path.basename(GITHUB_REPOSITORY), PR_NUMBER, message)
-		print("\nExiting the {} script after failure\n".format(os.path.basename(__file__)))
-		sys.exit(1)
-	elif os.environ.get("CHALLENGE_ERRORS") != "False" and GITHUB_EVENT_NAME == "push":
-		issue_title = "Errors are found in your repository after commit."
-		issue_body = os.environ.get("CHALLENGE_ERRORS")
-		create_issue_in_repo(GITHUB_AUTH_TOKEN, os.path.basename(GITHUB_REPOSITORY), issue_title, issue_body)
-		print("\nExiting the {} script after failure\n".format(os.path.basename(__file__)))
-		sys.exit(1)
+	is_valid, errors = check_for_errors()
+	if not is_valid:
+		if VALIDATION_STEP=="True" and check_if_pull_request():
+			pr_number = GITHUB_CONTEXT["event"]["number"]
+			add_pull_request_comment(GITHUB_AUTH_TOKEN, os.path.basename(GITHUB_REPOSITORY), pr_number, errors)
+			print("\nExiting the {} script after failure\n".format(os.path.basename(__file__)))
+			sys.exit(1)
+		else:
+			issue_title = "Following errors occurred while validating the challenge config:"
+			create_github_repository_issue(GITHUB_AUTH_TOKEN, os.path.basename(GITHUB_REPOSITORY), issue_title, errors)
+			print("\nExiting the {} script after failure\n".format(os.path.basename(__file__)))
+			sys.exit(1)
 
 	print("\nExiting the {} script after success\n".format(os.path.basename(__file__)))
-
